b99ab8c69ed3cc76363d5970a6b8ab58925a7f78
Dinhbaon <107369617+Dinhbaon@users.noreply.github.com>
2022-10-16 20:27:48 -0400

    Update 1_Chart_Donut_Gender.svelte

--- client/src/1_Chart_Donut_Gender.svelte
+++ client/src/1_Chart_Donut_Gender.svelte
@@ -163,6 +163,8 @@ Object.values(genderlist).forEach(function (x) { genderCount[x] = (genderCount[x
 let number  = Object.values(genderCount).reduce((a,b)=>a+b)
 
 
+var width = context.chart.width;
+var size = Math.round(width / 32);
 
 myChart = new Chart(ctx, {
     type: 'doughnut',
@@ -181,7 +183,7 @@ myChart = new Chart(ctx, {
     },options: {
     plugins: {datalabels:{
         font: {
-            size: 20
+            size: size
         }
     },title:{
         display: true, 
