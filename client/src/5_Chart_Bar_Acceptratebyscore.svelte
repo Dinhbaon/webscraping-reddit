c6161d78fc5983b1c6f4d92ab8c8a98e6e37e3f7
Dinhbaon <107369617+Dinhbaon@users.noreply.github.com>
2022-10-18 02:22:18 -0400

    change font

--- client/src/5_Chart_Bar_Acceptratebyscore.svelte
+++ client/src/5_Chart_Bar_Acceptratebyscore.svelte
@@ -194,8 +194,13 @@ myChartAveragescores = new Chart(ctx, {
 
         },
         datalabels:{
-            font: {
-                size: 20
+            font:              function(ctx) {
+                var width = ctx.chart.width;
+                var size = Math.round(width / 30 );
+
+                return {
+                    size: size
+                };
             }
         },title:{
             display: true, 
