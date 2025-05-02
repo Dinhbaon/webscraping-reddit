982ad5d8ab56becc6b129dc00439ed1cbea0ab64
Dinhbaon <dinhbao142@gmail.com>
2025-05-02 10:47:38 -0700

    added amplitude analyitics

--- webscraping_college_results/src/main.tsx
+++ webscraping_college_results/src/main.tsx
@@ -6,7 +6,8 @@ import { Analytics } from '@vercel/analytics/react';
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <html lang="en">
   <head>
-    <title>Next.js</title>
+    <title>Reddit College Results</title>
+    <script src="https://cdn.amplitude.com/libs/analytics-browser-2.11.1-min.js.gz"></script><script src="https://cdn.amplitude.com/libs/plugin-session-replay-browser-1.8.0-min.js.gz"></script><script>window.amplitude.add(window.sessionReplay.plugin({sampleRate: 1}));window.amplitude.init('2d8142fdc59dc3cc2f81f3e1d37be13c', {"autocapture":{"elementInteractions":true}});</script>
   </head>
   <body>
   <React.StrictMode>
